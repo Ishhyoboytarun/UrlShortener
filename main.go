78ee339b74a4bcf693b734a3f4c41246d1cc752c
Tarun Gusain <36428256+Ishhyoboytarun@users.noreply.github.com>
2023-04-20 11:23:09 +0530

    Adding dependency injection

--- main.go
+++ main.go
@@ -9,6 +9,10 @@ import (
 	"github.com/gorilla/mux"
 )
 
+type Dependencies struct {
+	UrlShortenerHandler *handler.UrlShortenerHandler
+}
+
 func main() {
 	r := mux.NewRouter()
 
@@ -18,4 +22,15 @@ func main() {
 	r.HandleFunc("/getShortenUrl", handler.UrlShortenerHandler.ShortenUrl).Methods("POST")
 
 	http.ListenAndServe(":8001", r)
+}
+
+func initializeDependencies() *Dependencies {
+	longToShort := make(map[string]string)
+	shortToLong := make(map[string]string)
+	urlShortenerRepo := repo.NewShortenUrlRepo(longToShort, shortToLong)
+	urlShortenerService := service.NewShortenUrlService(urlShortenerRepo)
+	urlShortenerHandler := handler.NewShortenUrlHandler(urlShortenerService)
+	return &Dependencies{
+		UrlShortenerHandler: urlShortenerHandler,
+	}
 }
